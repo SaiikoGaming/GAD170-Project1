40c8cb6e1b5a226de9f059eac76b6348818fc8a1
Saiiko <1033120@student.sae.edu.au>
2024-02-22 14:43:29 +1000

    no message

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    public KeyCode buttonPressed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +14,18 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(buttonPressed))
+        // checks to see if the button has pressed down
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Space bar pressed down, this runs once until released");
         }
-        if (Input.GetKeyUp(buttonPressed))
+        // checks to see if the button is pressed 
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            Debug.Log("space bar has been pressed then released, this wont run until the release");
+            Debug.Log("space bar has been pressed then released");
         }
-        if (Input.GetKey(buttonPressed))
+        // checks to see if the button is released
+        if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("The space bar is still being pressed, will run when space bar is down");
         }
