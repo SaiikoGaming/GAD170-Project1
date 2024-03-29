bff7e9b6d7578d4febe17c7f071f5f80b166dbef
Saiiko <1033120@student.sae.edu.au>
2024-03-29 23:55:53 +1000

    completed instantiation, destroying and This keyword

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public EnemyManager enemyManager;
+
+    private void Start()
+    {
+        //finding the enemymanager in the game
+        enemyManager = FindObjectOfType<EnemyManager>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            //destroying this when dead
+            if (enemyManager != null)
+            {
+                enemyManager.RemoveEnemy(this);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    // creating list and such
+    public GameObject enemyPrefab;
+    public List<Enemy> allEnemies = new List<Enemy>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 5; i++) 
+        {
+            //spawning in random locations
+            float x = Random.Range(-10f, 10f);
+            float y = Random.Range(-10f, 10f);
+            float z = Random.Range(-10f, 10f);
+            Vector3 spawnPosition = new Vector3(x, y, z);
+            GameObject clone = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            allEnemies.Add(clone.GetComponent<Enemy>());
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i = 0; i < allEnemies.Count; i++)
+            {
+                allEnemies[i].TakeDamage(20);
+            }
+        }
+    }
+
+    // removing an enemy
+    public void RemoveEnemy(Enemy enemyToRemove)
+    {
+        allEnemies.Remove(enemyToRemove);
+    }
+}
